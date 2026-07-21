7fd518727174a683269a1ac54ae580bbd6141958
homann09-hue <homann09@icloud.com>
2026-07-21 20:46:28 +0200

    Add jackpots.css to layout component

--- apps/player-web/src/app/layout.tsx
+++ apps/player-web/src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./wheel.css";
 import "./shop.css";
 import "./reels.css";
 import "./win.css";
+import "./jackpots.css";
 import { AgeGate } from "@/components/age-gate";
 import { LegalFooter } from "@/components/legal-footer";
 
